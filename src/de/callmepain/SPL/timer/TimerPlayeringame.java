adfc991a91ae44b548366a070863d03c6c9dd77c
Nils k <beta3001@googlemail.com>
2012-04-02 21:20:39 +0200

    2.7.8

--- src/de/callmepain/SPL/timer/TimerPlayeringame.java
+++ src/de/callmepain/SPL/timer/TimerPlayeringame.java
@@ -9,7 +9,6 @@ public class TimerPlayeringame implements Runnable{
 		}
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
-			plugin.Util.SPLBroadcast(plugin.Chatplayer + plugin.SPL_Player.get("1").getName() + plugin.Chattext + "Wartet noch immer auf einen Gegner!");
+			plugin.Util.SPLBroadcast(plugin.Chatplayer + plugin.SPL_Player.get("1").getName() + plugin.Chattext + " Wartet noch immer auf einen Gegner!");
 		}
 }
